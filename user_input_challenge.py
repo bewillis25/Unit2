f7c6a5ce64eaa28df495ff82cfb7098d9b67dede
bewillis25 <bewillis25@students.bmchs.org>
2022-01-27 09:35:21 -0600

    finished user input challenge

--- user_input_challenge.py
+++ user_input_challenge.py
@@ -12,14 +12,14 @@ Print out that many copies of the previous message on separate lines.
 (Hint: the string "\n" is the same as pressing the ENTER button)
 '''
 
-# Original year you will turn 100 program
+# Original year you will turn 100 program.
 name1 = input("What is your name? ")
 age1 = int(input("What is your age? "))
 current_year = int(2021)
 year_you_will_turn_100 = (current_year-age1)+(100)
 print(f"{name1}, you will turn 100 in the year {year_you_will_turn_100}.")
 
-# Copied year you will turn 100 program
+# Copied year you will turn 100 program.
 name2 = input("What is your name? ")
 age2 = int(input("What is your age? "))
 copy_number = int(input("Enter a number: "))

bd1b4a62ef7958854434ef26bbf652cd82957a57
unknown <bewillis25@students.gmail.com>
2022-02-04 19:41:03 -0600

    Lab 2.06

--- Lab_2.06.py
+++ Lab_2.06.py
@@ -10,8 +10,8 @@ Example 1
     while a < 10:
         print(a)
 
-prediction:
-actual:
+prediction: 0, and it will keep on printing this forever, unless a > 10.
+actual: Same as prediction
 
 Example 2
     a = 0
@@ -19,8 +19,8 @@ Example 2
         a = a + 1
         print(a)
 
-prediction:
-actual:
+prediction: 1 \n 2 \n 3 \n 4 \n 5 \n 6 \n 7 \n 8 \n 9 \n 10
+actual: Same as prediction
 
 2. In your Notebook
 -------------------
@@ -30,6 +30,7 @@ Create a set of test cases for the following sample code and predict the behavio
         a = input("Would you like to quit: ")
 
 test cases: 'y', 'n', 'q', 'cat'
+Code will terminate if a = 'y' or a = 'n', but if a = 'cat' or 'q' it will keep asking "Would you like to quit"
 
 3. Implement the Tic Tac Toe game using a while loop
 ----------------------------------------------------
@@ -52,3 +53,35 @@ Print the updated board out.
 You will not need to determine the winner at this point.
 (Copy and paste your previous tic-tac-toe version and modify the code to implement the above)
 '''
+
+# New Tic-Tac-Toe Game With While Loop
+a = ['1','2','3','4','5','6','7','8','9']
+print("This is a two player Tic-Tac-Toe game so there will be alternating turns. The board diagram is shown below. Enter the number of the square you wish to put your X or O.")
+print(f" {a[0]} | {a[1]} | {a[2]} \n --------- \n {a[3]} | {a[4]} | {a[5]} \n --------- \n {a[6]} | {a[7]} | {a[8]}")
+i = 1
+while i < 5:
+    p1_i_turn = int(input("Player 1, select a square: "))
+    a[p1_i_turn-1] = 'X'
+    board = f" {a[0]} | {a[1]} | {a[2]} \n --------- \n {a[3]} | {a[4]} | {a[5]} \n --------- \n {a[6]} | {a[7]} | {a[8]}"
+    print(board)
+    p2_i_turn = int(input("Player 2, select a square: "))
+    a[p2_i_turn-1] = 'O'
+    board = f" {a[0]} | {a[1]} | {a[2]} \n --------- \n {a[3]} | {a[4]} | {a[5]} \n --------- \n {a[6]} | {a[7]} | {a[8]}"
+    print(board)
+    i = i + 1  
+p1_i_turn = int(input("Player 1, select a square: "))
+a[p1_i_turn-1] = 'X'
+board = f" {a[0]} | {a[1]} | {a[2]} \n --------- \n {a[3]} | {a[4]} | {a[5]} \n --------- \n {a[6]} | {a[7]} | {a[8]}"
+print(board)
+
+    
+
+
+
+
+
+
+
+
+
+
